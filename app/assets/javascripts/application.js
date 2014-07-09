bebe4d553dd8f144e5c422f5f64ee7d6bcfc8b9d
sammykhaleel <sammykhaleel83@gmail.com>
2014-07-09 13:30:19 -0400

    first commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,10 @@
+
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
 // or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
+// 
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file.
 //
@@ -12,5 +13,17 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
 //= require turbolinks
 //= require_tree .
+
+
+//Flying Letters script- by Matthias (info@freejavascripts.f2s.com)
+// Modified by Twey for efficiency and compatibility
+//For this script and more, visit Dynamic Drive: http://www.dynamicdrive.com
+
+//Configure message to display. Use "$" for linebreak
+//By default, set to just grab the text from element with ID="fly"
+
+
+
