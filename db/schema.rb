a7471fa12ebbc1b2bce77082957ec0b59f68a75c
shushugah <mathsolver24@gmail.com>
2014-07-16 10:15:16 -0400

    modified seeds for testing

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140711184936) do
+ActiveRecord::Schema.define(version: 20140715152631) do
 
   create_table "services", force: true do |t|
     t.string   "name"
     t.string   "traffic"
-    t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,17 +2,10 @@ require 'open-uri'
 require 'nokogiri'
     @train_name_array=[]
     @train_status_array=[]
-    @train_description=[]
+    # @train_headline=[]
+    # @train_details=[]
     
     @doc = Nokogiri::XML(open("http://web.mta.info/status/serviceStatus.txt"))
-    
-def subway_list
-      @doc.xpath("//name").collect do |lines|
-        lines = lines.text
-        @lines = lines
-      end
-end
- 
  
   def descriptive_status
     @doc.xpath('//subway//line').each do |line|
@@ -21,48 +14,53 @@ end
       @train_name_array << train_name
       train_status = line.at_css("status").text
       @train_status_array << train_status
-      descriptive_status = line.xpath("text")
-      
-      html = Nokogiri::HTML(descriptive_status.text)
-      
-      html.css(".plannedWorkDetailLink").each do |detail|
-        planned_work_detail_link = detail.children.text
-        @train_description << planned_work_detail_link
-      end
- 
-      html.css(".plannedWorkDetail").each do |detail|
-        planned_work_detail = detail.children.text
-        @train_description << planned_work_detail
-      end
- 
-      if train_status == "GOOD SERVICE"
-        @train_description << "Hooray! Good train service today!"
-      else 
-        status_text = descriptive_status.text
-        cleaned_status = status_text.scan(/<br\/><br\/>(.+)<br\/><br\/>/m) # slightly working 
-        
-        new_clean_status = cleaned_status.flatten
- 
-        new_clean_status.each do |line_info|
-          puts line_info.gsub(/<(\/)*(\w)+(\s)*(\/)*>/m, "")
-        end 
-      end 
+    #   descriptive_status = line.xpath("text")
+    #   #this is the object with both headline/details
+    #   html = Nokogiri::HTML(descriptive_status.text)
+     
+    #   if train_status == "GOOD SERVICE"
+    #     @train_headline << "Hooray! Good train service today!"
+    #     @trail_details << "Relax"
+    #   else
+    #     html.css(".plannedWorkDetailLink").each do |detail|
+    #       planned_work_detail_link = detail.children.text
+    #       @train_headline << planned_work_detail_link
+    #     end    
+    #     html.css(".TitleServiceChange").each do |detail|
+    #       descriptive_detail = detail.children.text
+    #       @train_details << descriptive_detail
+    #     end
+
+    #     html.css(".TitleDelay").each do |detail|
+    #       descriptive_detail = detail.children.text
+    #       @train_details << descriptive_detail
+    #     end
+
+    #     html.css('.TitlePlannedWork').each do |detail|
+    #       descriptive_detail = detail.children.text
+    #       @train_details << descriptive_detail
+    #     end
+   
+    #     html.css(".plannedWorkDetail").each do |detail|
+    #       planned_work_detail = detail.children.text
+    #       @train_details << planned_work_detail
+    #   end 
+    # end
     end 
   end
 
 
   def output
-   @train_name_array.zip(@train_status_array, @train_description)
+   @train_name_array.zip(@train_status_array)
   end
 
-#Before it seeds, it deletes all previous seeds
 puts "deleting old seeds"
-Service.delete_all
 descriptive_status
+Service.delete_all
 puts "loading"
 output.each do |subway_array|
 
-  Service.create(name: subway_array[0], traffic: subway_array[1], description: subway_array[2])
+  Service.create(name: subway_array[0], traffic: subway_array[1])
   end
 puts Service.count
 puts "finished loading"
