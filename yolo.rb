4884174c5ba6caf9b88d703c8c44c28fa19c7b80
shushugah <mathsolver24@gmail.com>
2014-07-16 10:15:16 -0400

    modified seeds for testing

---
+++ yolo.rb
@@ -0,0 +1,53 @@
+	# @train_name_array=[]
+ #    @train_status_array=[]
+ #    @train_headline=[]
+ #    @train_details=[]
+    
+ #    @doc = Nokogiri::XML(open("http://web.mta.info/status/serviceStatus.txt"))
+ 
+ #  def descriptive_status
+ #    @doc.xpath('//subway//line').each do |line|
+      
+ #      train_name = line.at_css("/name").text
+ #      @train_name_array << train_name
+ #      train_status = line.at_css("status").text
+ #      @train_status_array << train_status
+ #      descriptive_status = line.xpath("text")
+ #      #this is the object with both headline/details
+ #      html = Nokogiri::HTML(descriptive_status.text)
+ #      binding.pry
+ #      if train_status == "GOOD SERVICE"
+ #        @train_headline << "Hooray! Good train service today!"
+ #        @trail_details << "Relax"
+ #      else
+ #        html.css(".plannedWorkDetailLink").each do |detail|
+ #          planned_work_detail_link = detail.children.text
+ #          @train_headline << planned_work_detail_link
+ #        end    
+ #        html.css(".TitleServiceChange").each do |detail|
+ #          descriptive_detail = detail.children.text
+ #          @train_details << descriptive_detail
+ #        end
+
+ #        html.css(".TitleDelay").each do |detail|
+ #          descriptive_detail = detail.children.text
+ #          @train_details << descriptive_detail
+ #        end
+
+ #        html.css('.TitlePlannedWork').each do |detail|
+ #          descriptive_detail = detail.children.text
+ #          @train_details << descriptive_detail
+ #        end
+   
+ #        html.css(".plannedWorkDetail").each do |detail|
+ #          planned_work_detail = detail.children.text
+ #          @train_details << planned_work_detail
+ #      end 
+ #    end
+ #    end 
+ #  end
+
+
+ #  def output
+ #   @train_name_array.zip(@train_status_array, @train_headline, @train_details)
+ #  end
