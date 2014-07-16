d0aecc7de2e710a3dabab49db221436aace1c348
shushugah <mathsolver24@gmail.com>
2014-07-16 15:19:50 -0400

    fixed order of migrations

--- db/migrate/20140701180242_devise_create_users.rb
+++ db/migrate/20140701180242_devise_create_users.rb
@@ -37,8 +37,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
 
     add_index :users, :email,                :unique => true
     add_index :users, :reset_password_token, :unique => true
-    add_index :users, :first_name,                :unique => true
-    add_index :users, :last_name,                :unique => true
+
 
 
     # add_index :users, :confirmation_token,   :unique => true
