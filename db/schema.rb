ccb0d390b535290fdf78ce29331a44d250ec3161
Yaritza <saturn540@gmail.com>
2014-07-17 16:09:19 -0400

    pulling group updates

--- db/schema.rb
+++ db/schema.rb
@@ -14,21 +14,15 @@
 ActiveRecord::Schema.define(version: 20140717200248) do
 
   create_table "alerts", force: true do |t|
-    t.datetime "start"
-    t.datetime "end"
-    t.integer  "user_id"
-    t.integer  "line_id"
-    t.integer  "recipient_id"
-    t.text     "sms"
-    t.text     "email"
+    t.string   "train_line"
+    t.time     "start_time"
+    t.time     "end_time"
+    t.boolean  "sms"
+    t.boolean  "email"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "last_sent"
   end
-
-  add_index "alerts", ["line_id"], name: "index_alerts_on_line_id"
-  add_index "alerts", ["recipient_id"], name: "index_alerts_on_recipient_id"
-  add_index "alerts", ["user_id"], name: "index_alerts_on_user_id"
 
   create_table "lines", force: true do |t|
     t.string   "name"
