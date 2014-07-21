88a3f68ff1313af96bc3ff9ade7c5487b9b530d9
shushugah <mathsolver24@gmail.com>
2014-07-21 16:23:39 -0400

    trying to pull yaritza files in spite of migration differences

---
+++ db/schema.rb
@@ -0,0 +1,74 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20140717200248) do
+
+  create_table "alerts", force: true do |t|
+    t.datetime "start"
+    t.datetime "end"
+    t.integer  "user_id"
+    t.integer  "line_id"
+    t.integer  "recipient_id"
+    t.text     "sms"
+    t.text     "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.datetime "last_sent"
+  end
+
+  add_index "alerts", ["line_id"], name: "index_alerts_on_line_id"
+  add_index "alerts", ["recipient_id"], name: "index_alerts_on_recipient_id"
+  add_index "alerts", ["user_id"], name: "index_alerts_on_user_id"
+
+  create_table "recipients", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "phone_number"
+    t.string   "email_address"
+    t.text     "email_template"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+  end
+
+  create_table "services", force: true do |t|
+    t.string   "name"
+    t.string   "traffic"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "phone_number"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["first_name"], name: "index_users_on_first_name", unique: true
+  add_index "users", ["last_name"], name: "index_users_on_last_name", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+end
