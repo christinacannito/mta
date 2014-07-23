fa5c1ce5902be51aaa8329984ae0dcca47ae0e26
Yaritza <saturn540@gmail.com>
2014-07-23 17:06:14 -0400

    Fix Devise Login

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140723145613) do
 
-
   create_table "alerts", force: true do |t|
     t.datetime "start"
     t.datetime "end"
@@ -29,6 +28,12 @@ ActiveRecord::Schema.define(version: 20140723145613) do
 
   add_index "alerts", ["recipient_id"], name: "index_alerts_on_recipient_id"
   add_index "alerts", ["user_id"], name: "index_alerts_on_user_id"
+
+  create_table "lines", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "recipients", force: true do |t|
     t.string   "first_name"
