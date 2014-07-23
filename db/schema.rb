8de202e066d80f3738bdb52515775fe709736658
Yaritza <saturn540@gmail.com>
2014-07-22 22:01:46 -0400

    stripping auth keys

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140723145613) do
+ActiveRecord::Schema.define(version: 20140722154717) do
 
   create_table "alerts", force: true do |t|
     t.datetime "start"
