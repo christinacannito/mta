7c185abadb5c9def6839f2a45ef8d17331a8171b
Christina Cannito <christinacannito@gmail.com>
2014-07-24 09:29:32 -0400

    blake committing christinas changes from last night (also fixed merge conflicts using cherry-pick)
    Conflicts:
    	app/models/twilio_wrapper.rb
    	app/views/alerts/new.html.erb

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,14 +11,12 @@ class ServiceSeeds
     @doc = Nokogiri::XML(open("http://web.mta.info/status/serviceStatus.txt"))
   end
 
-    @train_name_array=[]
-    @train_status_array=[]
-    # @train_headline=[]
-    # @train_details=[]
-    
-    @doc = Nokogiri::XML(open("http://web.mta.info/status/serviceStatus.txt"))
- 
+  def descriptive_status
+    @doc.xpath('//subway//line').each do |line|
 
+      train_name = line.at_css("/name").text
+
+      train_status = line.at_css("status").text
       @output_hash[train_name][:status] = train_status 
       super_descriptive = line.xpath("text")
 
@@ -36,40 +34,11 @@ class ServiceSeeds
           planned_work_detail = detail.children.text
           @output_hash[train_name][:details] = planned_work_detail
         end
-      @train_status_array << train_status
-    #   descriptive_status = line.xpath("text")
-    #   #this is the object with both headline/details
-    #   html = Nokogiri::HTML(descriptive_status.text)
-     
-    #   if train_status == "GOOD SERVICE"
-    #     @train_headline << "Hooray! Good train service today!"
-    #     @trail_details << "Relax"
-    #   else
-    #     html.css(".plannedWorkDetailLink").each do |detail|
-    #       planned_work_detail_link = detail.children.text
-    #       @train_headline << planned_work_detail_link
-    #     end    
-    #     html.css(".TitleServiceChange").each do |detail|
-    #       descriptive_detail = detail.children.text
-    #       @train_details << descriptive_detail
-    #     end
 
-    #     html.css(".TitleDelay").each do |detail|
-    #       descriptive_detail = detail.children.text
-    #       @train_details << descriptive_detail
-    #     end
-
-    #     html.css('.TitlePlannedWork').each do |detail|
-    #       descriptive_detail = detail.children.text
-    #       @train_details << descriptive_detail
-    #     end
-   
-    #     html.css(".plannedWorkDetail").each do |detail|
-    #       planned_work_detail = detail.children.text
-    #       @train_details << planned_work_detail
-    #   end 
-    # end
-
+        html.css(".TitleDelay").each do |detail| # working with this 
+          title_delay = detail.children.text
+          @output_hash[train_name][:title_delay] = title_delay
+        end
 
         html.css(".TitleServiceChange").each do |detail|
           title_service_change = detail.children.text
@@ -85,8 +54,11 @@ end
 
 puts "deleting old Service seeds"
 Service.destroy_all
+
 service.descriptive_status.each do |train_name, info|
+
   Service.create(name: train_name, traffic: info[:status], description: info[:header], more_detail: info[:details])
 end
+
 
 puts "Updated service information"
