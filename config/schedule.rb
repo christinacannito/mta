f8e70b9d81fc0d87badbdb6a053a50fcd51a4fe9
Christina Cannito <christinacannito@gmail.com>
2014-07-28 16:37:35 -0400

    slight changes with views

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,3 @@
- set :environment, "development"
- set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
 # Use this file to easily define all of your cron jobs.
 #
 # It's helpful, but not entirely necessary to understand cron before proceeding.
@@ -7,26 +5,20 @@
 
 # Example:
 #
-
-set :output, "/path/to/my/cron_log.log"
-
-every 1.minute do 
-  rake "db:seed"
-end
-
-# # every 1.minute do 
-# # rake "sms"
-# # end
-# every 1.minute do 
-# rake "db:seed"
+# set :output, "/path/to/my/cron_log.log"
+#
+# every 2.hours do
+#   command "/usr/bin/some_great_command"
+#   runner "MyModel.some_method"
+#   rake "some:great:rake:task"
 # end
-
-# every 1.minute do 
-# 	rake "sms"
-# end
-# #
+#
 # every 4.days do
 #   runner "AnotherModel.prune_old_records"
 # end
 
 # Learn more: http://github.com/javan/whenever
+
+every 1.minutes do 
+	rake "db:seed"
+end 
