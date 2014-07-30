4b154e9fbca6a7b6ab42d82c4add562dd2568c6b
shushugah <mathsolver24@gmail.com>
2014-07-30 17:44:15 -0400

    corrected image paths, and modified precompile file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,14 +82,14 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
-  config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['sammypinterest'],
-    :access_key_id => ENV['AKIAIM67LCNDQ6I5ZQ2Q'],
-    :secret_access_key => ENV['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']
-  }
-  # :path =&gt; ":class/:id/:basename_:style.:extension",
-  #         :url =&gt; ":s3_sg_url"
-}
+  # config.paperclip_defaults = {
+  # :storage => :s3,
+  # :s3_credentials => {
+  #   :bucket => ENV["S3_SID"],
+  #   :access_key_id => ENV["sw3_access_key_id"],
+  #   :secret_access_key => ENV["sw3_secret_access_key"]
+  # }
+  # # :path =&gt; ":class/:id/:basename_:style.:extension",
+  # #         :url =&gt; ":s3_sg_url"
+
 end
