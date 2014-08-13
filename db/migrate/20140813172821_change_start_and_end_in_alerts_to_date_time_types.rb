04e58f99996a534df9ce381e0b2cbb44e07b416b
shushugah <mathsolver24@gmail.com>
2014-08-13 14:10:27 -0400

    made the time_relevance method work, and modified front end of DateTime objects

---
+++ db/migrate/20140813172821_change_start_and_end_in_alerts_to_date_time_types.rb
@@ -0,0 +1,7 @@
+class ChangeStartAndEndInAlertsToDateTimeTypes < ActiveRecord::Migration
+  def change
+  	change_column :alerts, :last_sent, :datetime
+  	change_column :alerts, :end, :datetime
+  	change_column :alerts, :start, :datime
+  end
+end
