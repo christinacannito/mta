a4b01149cfd6dfd2a620c616da7c8b85b79e8822
Yaritza <saturn540@gmail.com>
2014-08-14 14:16:22 -0400

    added a migration to correct the alert end time datatype to datetime

--- db/migrate/20140813172821_change_start_and_end_in_alerts_to_date_time_types.rb
+++ db/migrate/20140813172821_change_start_and_end_in_alerts_to_date_time_types.rb
@@ -2,6 +2,6 @@ class ChangeStartAndEndInAlertsToDateTimeTypes < ActiveRecord::Migration
   def change
   	change_column :alerts, :last_sent, :datetime
   	change_column :alerts, :end, :datetime
-  	change_column :alerts, :start, :datime
+  	change_column :alerts, :start, :datetime
   end
 end
